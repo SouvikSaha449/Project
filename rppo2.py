850da65c64ce4b93fe91b32009b7281490cd8626
Souvik Saha <souviksaha412@gmail.com>
2023-09-10 13:54:45 +0530

    This is the rppo code using string . This code is hitting the recursion depth limit of python. If the input string is long. The tested input string for which it is not hitting recursion depth is  like birds in flight, guiding us through darkness's slight.

--- rppo2.py
+++ rppo2.py
@@ -1,10 +1,8 @@
-#This is the version which will fully generate blocks until the size of the block
+import math
 
-def generate_blocks(source_block):
+def generate_blocks(source_block, num_iterations):
     intermediate_blocks = []
     intermediate_blocks.append(source_block.copy())
-
-    num_iterations = len(source_block)
 
     def generate_block_recursive(block, remaining_iterations):
         if remaining_iterations == 0:
@@ -22,24 +20,78 @@ def generate_blocks(source_block):
 
     return intermediate_blocks
 
+def encrypt(source_block, block_number, num_iterations):
+    intermediate_blocks = generate_blocks(source_block, num_iterations)
+    return intermediate_blocks[block_number]
+
+def decrypt(final_block, block_number, num_iterations):
+    if block_number >= len(final_block):
+        return []
+
+    decrypted_blocks = []
+
+    def generate_block_recursive(block, remaining_iterations):
+        if remaining_iterations == 0:
+            return
+        new_block = block.copy()
+        for j in range(len(block)):
+            xor_result = 0
+            for k in range(j + 1):
+                xor_result ^= block[k]
+            new_block[j] = xor_result
+        decrypted_blocks.append(new_block)
+        generate_block_recursive(new_block, remaining_iterations - 1)
+
+    generate_block_recursive(final_block, num_iterations - block_number)
+
+    return decrypted_blocks
+
+def string_to_binary(string):
+    binary_values = []
+    for char in string:
+        ascii_value = ord(char)
+        binary_value = format(ascii_value, '08b')  # Convert ASCII to 8-bit binary
+        binary_values.extend([int(bit) for bit in binary_value])
+    return binary_values
+
+def binary_to_string(binary_values):
+    binary_string = ''.join(map(str, binary_values))
+    chars = [binary_string[i:i+8] for i in range(0, len(binary_string), 8)]
+    return ''.join([chr(int(char, 2)) for char in chars])
+
 def main():
-    size = int(input("Enter the size of the source block: "))
-    source_block = []
-    for i in range(size):
-        bit = int(input(f"Enter bit {i + 1} (0 or 1) for the source block: "))
-        source_block.append(bit)
+    input_string = input("Enter a string to encrypt: ")
+    source_block = string_to_binary(input_string)
+    size = len(source_block)
 
-    intermediate_blocks = generate_blocks(source_block)
+    # Calculate the number of iterations based on the size of the source block
+    num_iterations = 2 ** math.ceil(math.log2(size))
 
-    # Rename the initial block to "Source Block"
-    intermediate_blocks[0] = source_block
+    block_number = int(input("Enter the block number of encryption: "))
 
-    # Print the "Source Block"
-    print(f'Source Block: {source_block}')
+    intermediate_blocks = generate_blocks(source_block, num_iterations)
 
-    # Print the other intermediate blocks
-    for i, block in enumerate(intermediate_blocks[1:], start=1):
-        print(f'Block {i}: {block}')
+    print(f'Source String: {input_string}')
+    print(f'\nSource ASCII Values: {[ord(char) for char in input_string]}\n')
+
+    print(f'Source Block (Binary): {source_block}\n')
+
+    for i, block in enumerate(intermediate_blocks[1:block_number + 1], start=1):
+        print(f'Encrypted Block {i}: {block}\n')
+
+    encrypted_block = encrypt(source_block, block_number, num_iterations)
+    encrypted_string = binary_to_string(encrypted_block)
+    print(f'\nEncrypted ASCII Values: {[ord(char) for char in encrypted_string]}\n')
+    print(f'\nEncrypted String: {encrypted_string}')
+    
+    decrypted_blocks = decrypt(encrypted_block, block_number, num_iterations)
+
+    for i, block in enumerate(decrypted_blocks):
+        print(f'Decrypted Block {i + block_number + 1}: {block}\n')
+
+    decrypted_string = binary_to_string(decrypted_blocks[-1])
+    print(f'\nDecrypted ASCII Values: {[ord(char) for char in decrypted_string]}')
+    print(f'\nDecrypted String: {decrypted_string}')
 
 if __name__ == "__main__":
     main()
