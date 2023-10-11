a30c1b77b1230fe943af0fea1cf92e4cef06912c
Souvik Saha <souviksaha412@gmail.com>
2023-10-11 22:15:06 +0530

    Rppo code with unicode 64 encoding

--- rppo4.py
+++ rppo4.py
@@ -55,16 +55,15 @@ def string_to_binary(string):
         binary_values.extend([int(bit) for bit in binary_value])
     return binary_values
 
-
-
 def binary_to_string(binary_values):
-    # Convert binary values to bytes
     bytes_list = [binary_values[i:i+8] for i in range(0, len(binary_values), 8)]
-    bytes_data = bytearray([int(''.join(map(str, byte)), 2) for byte in bytes_list])
-
-    # Convert bytes to the original UTF-8 encoded string
-    return bytes_data.decode('utf-8', errors='ignore')
-
+    utf8_bytes = bytearray([int(''.join(map(str, byte)), 2) for byte in bytes_list])
+    
+    try:
+        return utf8_bytes.decode('utf-8')
+    except UnicodeDecodeError:
+        # Handle invalid characters by ignoring them or replacing them
+        return utf8_bytes.decode('utf-8', errors='ignore')
 
 
 def main():
@@ -80,8 +79,6 @@ def main():
     intermediate_blocks = generate_blocks(source_block, num_iterations)
 
     print(f'Source String: {input_string}')
-    print(f'\nSource Unicode-64 Values: {[ord(char) for char in input_string]}\n')
-
     print(f'Source Block (Binary): {source_block}\n')
 
     for i, block in enumerate(intermediate_blocks[1:block_number + 1], start=1):
@@ -89,17 +86,17 @@ def main():
 
     encrypted_block = encrypt(source_block, block_number, num_iterations)
     encrypted_string = binary_to_string(encrypted_block)
-    print(f'\nEncrypted Unicode-64 Values: {[ord(char) for char in encrypted_string]}\n')
-    print(f'\nEncrypted String: {encrypted_string}')
+    print(f'Encrypted String: {encrypted_string}')
     
     decrypted_blocks = decrypt(encrypted_block, block_number, num_iterations)
 
     for i, block in enumerate(decrypted_blocks):
-        print(f'\nDecrypted Block {i + block_number + 1}: {block}\n')
+        print(f'Decrypted Block {i + block_number + 1}: {block}\n')
 
+    print(f'Source String: {input_string}')
+    print(f'Encrypted String: {encrypted_string}')
     decrypted_string = binary_to_string(decrypted_blocks[-1])
-    print(f'\nDecrypted Unicode-64 Values: {[ord(char) for char in decrypted_string]}')
-    print(f'\nDecrypted String: {decrypted_string}')
+    print(f'Decrypted String: {decrypted_string}')
 
 if __name__ == "__main__":
     main()
