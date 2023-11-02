09db6fa97c82b8e12b457032d4f0261a2c7326b5
Souvik Saha <souviksaha412@gmail.com>
2023-11-02 14:44:00 +0530

    This code changes the recursion depth limit

--- syst.py
+++ syst.py
@@ -1,10 +1,5 @@
 import sys
 
-# Set the desired recursion depth limit
-new_recursion_limit = 10000  # Change this to the desired limit
-
-try:
-    sys.setrecursionlimit(new_recursion_limit)
-    print("New Recursion Limit Set")
-except Exception as e:
-    print(f"Setting recursion limit failed: {e}")
+# Set a new recursion depth limit
+new_depth_limit = 100000 # Adjust to your desired limit
+sys.setrecursionlimit(new_depth_limit)
