4b70f35efafe0ad85b5178b380440361995b3662
SouvikSaha449 <souviksaha412@gmail.com>
2023-11-16 14:46:17 +0530

    This code performs second best

--- rppo5.py
+++ rppo5.py
@@ -3,85 +3,74 @@ import time
 import base64
 import sys
 
-new_depth_limit = 10000000 
+new_depth_limit = 100000
 sys.setrecursionlimit(new_depth_limit)
 
-def generate_blocks(source_block, num_iterations, operation_counts):
-    intermediate_blocks = [source_block.copy()]
+def generate_blocks(source_block, num_iterations, target_block_number, intermediate_blocks=None):
+    if intermediate_blocks is None:
+        intermediate_blocks = [source_block]
 
-    def generate_block_recursive(block, remaining_iterations):
-        if remaining_iterations == 0:
-            return
+    def generate_block_recursive(block, remaining_iterations, operation_count=0):
+        nonlocal intermediate_blocks
+
+        if remaining_iterations == 0 or len(intermediate_blocks) > target_block_number:
+            return operation_count
+
         xor_result = 0
-        new_block = block.copy()
-        operation_count = 0  # Count operations in this iteration
         for i in range(len(block)):
             xor_result ^= block[i]
-            new_block[i] = xor_result
+            block[i] = xor_result
             operation_count += 1
-        intermediate_blocks.append(new_block)
-        operation_counts.append(operation_count)
-        generate_block_recursive(new_block, remaining_iterations - 1)
 
-    generate_block_recursive(source_block, num_iterations)
+        intermediate_blocks.append(block)
+        return generate_block_recursive(block, remaining_iterations - 1, operation_count)
 
-    return intermediate_blocks
+    total_operations = generate_block_recursive(source_block, num_iterations)
+    return intermediate_blocks, total_operations
 
 def encrypt(source_block, block_number, num_iterations):
-    operation_counts = []  # To count operations
-    intermediate_blocks = generate_blocks(source_block, num_iterations, operation_counts)
+    intermediate_blocks, total_operations = generate_blocks(source_block, num_iterations, block_number)
     if block_number < len(intermediate_blocks):
-        return intermediate_blocks[block_number], sum(operation_counts[:block_number + 1])
+        return intermediate_blocks[block_number], total_operations
     else:
         return [], 0
 
 def decrypt(final_block, block_number, num_iterations):
-    operation_counts = []  # To count operations
     if block_number >= len(final_block):
         return [], 0
 
-    decrypted_blocks = []
+    decrypted_blocks = [final_block]
 
-    def generate_block_recursive(block, remaining_iterations):
+    def generate_block_recursive(block, remaining_iterations, operation_count=0):
+        nonlocal decrypted_blocks
+
         if remaining_iterations == 0:
-            return
+            return operation_count
+
         xor_result = 0
-        new_block = block.copy()
-        operation_count = 0  # Count operations in this iteration
         for i in range(len(block)):
             xor_result ^= block[i]
-            new_block[i] = xor_result
+            block[i] = xor_result
             operation_count += 1
-        decrypted_blocks.append(new_block)
-        operation_counts.append(operation_count)
-        generate_block_recursive(new_block, remaining_iterations - 1)
 
-    generate_block_recursive(final_block, num_iterations - block_number)
+        decrypted_blocks.append(block)
+        return generate_block_recursive(block, remaining_iterations - 1, operation_count)
 
-    return decrypted_blocks, sum(operation_counts)
+    total_operations = generate_block_recursive(final_block, num_iterations - block_number)
+    return decrypted_blocks, total_operations
 
 def string_to_binary(string):
-    utf8_bytes = string.encode('utf-8')
-    binary_values = bytearray()
-    for byte in utf8_bytes:
-        binary_value = format(byte, '08b')  # Convert byte to 8-bit binary
-        binary_values.extend([int(bit) for bit in binary_value])
-    return binary_values
+    return (int(bit) for byte in string.encode('utf-8') for bit in f"{byte:08b}")
 
 def binary_to_string(binary_values):
-    # Ensure the length of binary_values is a multiple of 8
     if len(binary_values) % 8 != 0:
         raise ValueError("Binary values length must be a multiple of 8 for proper decoding.")
 
-    # Convert binary values to bytes
-    bytes_list = [binary_values[i:i+8] for i in range(0, len(binary_values), 8)]
-    bytes_data = bytearray([int(''.join(map(str, byte)), 2) for byte in bytes_list])
-
-    # Convert bytes to the original UTF-8 encoded string
+    bytes_data = bytes(int(''.join(map(str, binary_values[i:i+8])), 2) for i in range(0, len(binary_values), 8))
     return bytes_data.decode('utf-8', errors='ignore')
 
 def main():
-    input_file = 'input.txt'  # Change to the actual input file path
+    input_file = 'input2.txt'  # Change to the actual input file path
     encrypted_output_file = 'encrypted.txt'  # Change to the desired encrypted output file path
     decrypted_output_file = 'decrypted.txt'  # Change to the desired decrypted output file path
 
@@ -90,15 +79,13 @@ def main():
         input_string = file.read()
 
     # Convert the input string to the source block
-    source_block = string_to_binary(input_string)
+    source_block = list(string_to_binary(input_string))
 
     size = len(source_block)  # Size of the source block in bits
     num_iterations = 2 ** math.ceil(math.log2(size))
 
     # Prompt for the block number
     block_number = int(input("Enter the block number for encryption: "))
-
-    intermediate_blocks = generate_blocks(source_block, num_iterations, [])
 
     # Encryption
     start_time = time.time()
@@ -126,17 +113,13 @@ def main():
     with open(decrypted_output_file, 'w', encoding='utf-8') as decrypted_file:
         decrypted_file.write(decrypted_string)
 
-    print("Encryption and decryption completed.")
-    print("Encrypted Base64 String:")
-    print(encrypted_base64)
-    print("Decrypted String:")
-    print(decrypted_string)
     print(f'Size of Source Block: {size} bits')
     print(f'Encryption Time: {encryption_time:.4f} seconds')
     print(f'Decryption Time: {decryption_time:.4f} seconds')
 
     print(f'Number of XOR Operations (Encryption): {encryption_operations}')
     print(f'Number of XOR Operations (Decryption): {decryption_operations}')
+
 
 if __name__ == "__main__":
     main()
