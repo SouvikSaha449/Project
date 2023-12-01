54e298c9b35481a2a8e1c22389f169e9cb2b7b9c
SouvikSaha449 <souviksaha412@gmail.com>
2023-12-01 23:26:59 +0530

    This version processes the data in chunks for both encryption and decryption, helping to reduce memory usage.  You can adjust the block_size parameter to find an optimal balance   between memory usage and performance for your specific use case.   Need to investigate the code more.

--- rppo6.py
+++ rppo6.py
@@ -7,109 +7,129 @@ import numpy as np
 new_depth_limit = 100000
 sys.setrecursionlimit(new_depth_limit)
 
-def generate_blocks(source_block, num_iterations, target_block_number):
-    intermediate_blocks = np.empty((num_iterations, len(source_block)), dtype=int)
-    intermediate_blocks[0] = source_block
 
-    def generate_block_recursive(i, remaining_iterations, operation_count=0):
-        nonlocal intermediate_blocks
+def generate_blocks(source_block, num_iterations, target_block_number, block_size=1024):
+    intermediate_blocks = [source_block]
+    intermediate_blocks_len = len(intermediate_blocks)
 
-        if i == num_iterations or i > target_block_number:
+    def generate_block_recursive(block, remaining_iterations, operation_count=0):
+        nonlocal intermediate_blocks, intermediate_blocks_len
+
+        if remaining_iterations == 0 or intermediate_blocks_len > target_block_number:
             return operation_count
 
-        xor_result = np.bitwise_xor.reduce(intermediate_blocks[i - 1], axis=0)
-        intermediate_blocks[i] = np.bitwise_xor(intermediate_blocks[i - 1], xor_result)
-        operation_count += len(intermediate_blocks[i])
+        xor_result = np.bitwise_xor.reduce(block, axis=0)
+        block = np.bitwise_xor(block, xor_result)
+        operation_count += len(block)
 
-        return generate_block_recursive(i + 1, remaining_iterations - 1, operation_count)
+        intermediate_blocks.append(block)
+        intermediate_blocks_len += 1
+        return generate_block_recursive(block, remaining_iterations - 1, operation_count)
 
-    total_operations = generate_block_recursive(1, num_iterations)
-    return intermediate_blocks[:target_block_number + 1], total_operations
+    chunk_size = min(block_size, num_iterations)
+    for i in range(0, num_iterations, chunk_size):
+        total_operations = generate_block_recursive(
+            intermediate_blocks[-1], chunk_size)
+        if intermediate_blocks_len > target_block_number:
+            break
+
+    return intermediate_blocks, total_operations
+
 
 def encrypt(source_block, block_number, num_iterations):
-    intermediate_blocks, total_operations = generate_blocks(source_block, num_iterations, block_number)
+    intermediate_blocks, total_operations = generate_blocks(
+        source_block, num_iterations, block_number)
     if block_number < len(intermediate_blocks):
         return intermediate_blocks[block_number], total_operations
     else:
-        return np.empty((0, len(source_block)), dtype=int), 0
+        return [], 0
 
-def decrypt(final_block, block_number, num_iterations):
-    if block_number >= len(final_block):
-        return np.empty((0, final_block.shape[1]), dtype=int), 0
 
-    decrypted_blocks = np.empty((num_iterations - block_number, final_block.shape[1]), dtype=int)
-    decrypted_blocks[0] = final_block[block_number]
+def decrypt(final_block, block_number, num_iterations, block_size=1024):
+    if block_number >= num_iterations:
+        print("Decryption failed. Block number out of range.")
+        return [], 0
 
-    def generate_block_recursive(i, remaining_iterations, operation_count=0):
-        nonlocal decrypted_blocks
+    decrypted_blocks = [final_block]
+    intermediate_blocks_len = len(decrypted_blocks)
 
-        if i == num_iterations - block_number:
+    def generate_block_recursive(block, remaining_iterations, operation_count=0):
+        nonlocal decrypted_blocks, intermediate_blocks_len
+
+        if remaining_iterations == 0:
             return operation_count
 
-        xor_result = np.bitwise_xor.reduce(decrypted_blocks[i - 1], axis=0)
-        decrypted_blocks[i] = np.bitwise_xor(decrypted_blocks[i - 1], xor_result)
-        operation_count += len(decrypted_blocks[i])
+        xor_result = np.bitwise_xor.reduce(block, axis=0)
+        block = np.bitwise_xor(block, xor_result)
+        operation_count += len(block)
 
-        return generate_block_recursive(i + 1, remaining_iterations - 1, operation_count)
+        decrypted_blocks.append(block)
+        intermediate_blocks_len += 1
+        return generate_block_recursive(block, remaining_iterations - 1, operation_count)
 
-    total_operations = generate_block_recursive(1, num_iterations - block_number)
-    return decrypted_blocks[:num_iterations - block_number], total_operations
+    chunk_size = min(block_size, num_iterations - block_number)
+    for i in range(0, num_iterations - block_number, chunk_size):
+        total_operations = generate_block_recursive(
+            decrypted_blocks[-1], chunk_size)
+        if intermediate_blocks_len > block_number:
+            break
+
+    return decrypted_blocks, total_operations
+
 
 def string_to_binary(string):
-    return np.array([int(bit) for byte in string.encode('utf-8') for bit in f"{byte:08b}"])
+    return np.array([int(bit) for byte in string.encode('utf-8') for bit in f"{byte:08b}"], dtype=np.uint8)
+
 
 def binary_to_string(binary_values):
     if len(binary_values) % 8 != 0:
         raise ValueError("Binary values length must be a multiple of 8 for proper decoding.")
 
-    bytes_data = bytes(int(''.join(map(str, binary_values[i:i+8])), 2) for i in range(0, len(binary_values), 8))
+    bytes_data = bytes(
+        int(''.join(map(str, binary_values[i:i + 8])), 2) for i in range(0, len(binary_values), 8))
     return bytes_data.decode('utf-8', errors='ignore')
 
-def main():
-    input_file = 'input4.txt'  # Change to the actual input file path
-    encrypted_output_file = 'encrypted.txt'  # Change to the desired encrypted output file path
-    decrypted_output_file = 'decrypted.txt'  # Change to the desired decrypted output file path
 
-    # Read the input from a file
+def main():
+    input_file = 'input5.txt'  # Change to the actual input file path
+    encrypted_output_file = 'encrypted.txt'
+    decrypted_output_file = 'decrypted.txt'
+
     with open(input_file, 'r', encoding='utf-8') as file:
         input_string = file.read()
 
-    # Convert the input string to the source block
     source_block = np.array(list(string_to_binary(input_string)))
 
-    size = len(source_block)  # Size of the source block in bits
-    num_iterations = 2 ** math.ceil(math.log2(size))
+    size_kb = source_block.nbytes / 1024
+    num_iterations = 2 ** math.ceil(math.log2(len(source_block)))
 
-    # Prompt for the block number
     block_number = int(input("Enter the block number for encryption: "))
 
-    # Encryption
     start_time = time.time()
-    encrypted_block, encryption_operations = encrypt(source_block, block_number, num_iterations)
+    encrypted_block, encryption_operations = encrypt(
+        source_block, block_number, num_iterations)
     end_time = time.time()
     encryption_time = end_time - start_time
 
-    # Encode the encrypted data as base64
-    encrypted_base64 = base64.b64encode(bytes(encrypted_block)).decode('utf-8')
+    encrypted_base64 = base64.b64encode(
+        bytes(encrypted_block)).decode('utf-8')
 
-    # Write the encrypted base64 string to the encrypted output file
     with open(encrypted_output_file, 'w', encoding='utf-8') as encrypted_file:
         encrypted_file.write(encrypted_base64)
 
-    # Decryption
     start_time = time.time()
-    decrypted_blocks, decryption_operations = decrypt(encrypted_block, block_number, num_iterations)
+    decrypted_blocks, decryption_operations = decrypt(
+        encrypted_block, block_number, num_iterations)
     end_time = time.time()
     decryption_time = end_time - start_time
 
-    # Convert the decrypted binary data to a string
-    decrypted_string = binary_to_string(bytes(decrypted_blocks[-1]))
+    decrypted_string = binary_to_string(
+        bytes(decrypted_blocks[-1]))
 
-    # Write the decrypted string to the decrypted output file
     with open(decrypted_output_file, 'w', encoding='utf-8') as decrypted_file:
         decrypted_file.write(decrypted_string)
 
-    print(f'Size of Source Block: {size} bits')
+    print(f'Size of Source Block: {size_kb:.2f} KB')
     print(f'Encryption Time: {encryption_time:.4f} seconds')
     print(f'Decryption Time: {decryption_time:.4f} seconds')
 
@@ -117,6 +137,7 @@ def main():
     print(f'Number of XOR Operations (Decryption): {decryption_operations}')
 
     print("Encryption and decryption completed.")
+
 
 if __name__ == "__main__":
     main()
